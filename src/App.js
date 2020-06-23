981b9901e5fc3f81fc73b7214dc92dc0bfaae09f
lottieh <cdmhouse@sky.com>
2020-06-23 18:39:52 +0100

    Standard Text removed and spinning image changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import belguimGarden from './belguimGarden.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={belguimGarden} className="App-logo" alt="logo" />
+        <h1>
+          Charlotte House Portfolio
+        </h1>
+        
       </header>
     </div>
   );
