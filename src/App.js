73892c8b197eff1ffa6bab55322435e723b464df
Charlotte House <cdmhouse@sky.com>
2020-06-24 21:49:54 +0100

    component name amended

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import pageHeader from './Homepage/pageHeader';
+import PageHeader from './Homepage/pageHeader';
 
 class App extends React.Component{
   render(){
   return (
     <div className="App">
    <header>
-   <pageHeader/>  
+   <PageHeader/>  
    wordss
-   
+
      </header> 
     </div>
   );
