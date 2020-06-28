371e795f30695fe9fd7c5d2a83441f7239ba9292
lottieh <cdmhouse@sky.com>
2020-06-28 17:56:08 +0100

    My face imported

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import React from 'react';
 import './App.css';
 import PageHeader from './Homepage/pageHeader';
 import AboutMe from './Homepage/AboutMe';
+import Myface from './Homepage/Myface';
 
-class App extends React.Component{
-  render(){
-  return (
-    <div className="App">
-   <header>
-   <PageHeader/>  
-   <AboutMe />
-   
-   <div className="boxed"> 
- words test 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+      <div className="App">
+        <header>
+          <PageHeader />
+
+
+          <div className="row">
+            <Myface />
+            <AboutMe />
+          </div>
+
+
+          <div className="boxed">
+            words test
    </div>
-     </header> 
-    </div>
-  );
-};
+        </header>
+      </div>
+      </div>
+    );
+  };
 }
 
 export default App;
