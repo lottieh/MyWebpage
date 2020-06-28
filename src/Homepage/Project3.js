27ee756c6476f12fa61d738345c2f9a1ac7def41
lottieh <cdmhouse@sky.com>
2020-06-28 18:46:04 +0100

    create component

---
+++ src/Homepage/Project3.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import gears from './gears.png'
+
+class Project3 extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+                <div className="project-title"> Project 3 - Rock Paper Scissors </div>
+                <img src={gears} alt="gears filler picture" width="400" className="boxed">
+
+                </img>
+
+            </div>
+        )
+    }
+}
+
+
+export default Project3
---
+++ src/Homepage/Project4.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import gears from './gears.png'
+
+class Project4 extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+                <div className="project-title"> Project 4 - My Portfolio </div>
+                <img src={gears} alt="gears filler picture" width="400" className="boxed">
+
+                </img>
+
+            </div>
+        )
+    }
+}
+
+
+export default Project4
