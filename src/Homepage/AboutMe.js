c7de11f9769ad60ba8ea7e36f9b9dbc9e52a52ef
lottieh <cdmhouse@sky.com>
2020-06-28 16:39:49 +0100

    AboutMe Section started

---
+++ src/Homepage/AboutMe.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class AboutMe extends React.Component {
+    
+    render() {
+        return (
+          <div className = "boxed">
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Vivamus tincidunt tempor turpis, in rhoncus turpis euismod non. 
+Donec sed arcu sed magna gravida maximus sed non ex. 
+Etiam porta aliquet magna, sed blandit purus. 
+Cras ac felis ut risus porta dictum. 
+Aenean id ultrices nunc. Nam eget nulla quis lorem commodo gravida. 
+Vivamus fringilla elementum neque, sit amet lacinia risus euismod pulvinar. 
+Maecenas tristique eleifend est sit amet vehicula. 
+Aenean ac elit ut eros sagittis pretium ac eget ligula. 
+Nulla ullamcorper, dolor ut varius lacinia, urna mauris tristique dolor, 
+malesuada condimentum lorem ligula vel lacus. In fermentum, elit id vulputate 
+pharetra, leo diam congue orci, sed fringilla sapien mauris sed est. Quisque ut maximus elit, et lacinia lacus. 
+Pellentesque tincidunt turpis sed nisi pulvinar aliquam.  
+          </div> 
+
+        
+        )}}
+
+
+    export default AboutMe
