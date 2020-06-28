239e6cd404a7bc4a34d0b01de6da8b4a005f635b
lottieh <cdmhouse@sky.com>
2020-06-28 17:55:47 +0100

    Add Section for my picture

---
+++ src/Homepage/Myface.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FlowerBox from './FlowerBox.jpg'
+
+class Myface extends React.Component {
+    
+    render() {
+        return (
+             
+            <img src={FlowerBox} alt="Flowered Box" width="400" height="400" className= "boxed" col-md-6>
+
+            </img>
+        
+        )}}
+
+
+    export default Myface
