4a7568c4c88ce90897f928b242ee72ab1c24c5fa
Charlotte House <cdmhouse@sky.com>
2020-07-01 21:23:56 +0100

    import Github and Linkedin Logos

--- src/Homepage/Footer.js
+++ src/Homepage/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+
 
 class Footer extends React.Component {
     
@@ -10,6 +12,9 @@ class Footer extends React.Component {
 
             <h6> © Charlotte House July 2020</h6>
             <FontAwesomeIcon icon ={faCoffee}/>
+            <FontAwesomeIcon icon ={faGithub}  size="3x" />
+            <FontAwesomeIcon icon ={faLinkedin}  size="3x"  />
+
 
 {/* // todo - Add in git hub logo and link
 // Todo - Add Linkedin logo and link */}
