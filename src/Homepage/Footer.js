cfe74e12197e6572550eb4f86958592eaef0d31c
Charlotte House <cdmhouse@sky.com>
2020-07-01 19:43:16 +0100

    Footer section started

---
+++ src/Homepage/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    
+    render() {
+        return (
+           <div className ="Footer">
+
+            <h6> © Charlotte House July 2020</h6>
+
+//todo - Add in git hub logo and link
+// Todo - Add Linkedin logo and link
+
+        </div>
+        )}}
+
+
+    export default Footer
